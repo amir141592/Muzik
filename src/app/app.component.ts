3e22234aa67c01f2f5a95462967769af27461544
Amir Allahdadian <amir.allahdadian@gmail.com>
2024-01-07 22:03:03 +0330

    designed right menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { HeaderComponent } from './header/header.component';
 import { EventSliderComponent } from './event-slider/event-slider.component';
 import { SongSliderComponent } from './song-slider/song-slider.component';
 import { PlayerComponent } from './player/player.component';
+import { SamuraiButtonDirective } from '@kuro-samurai/ngx-samurai-button';
+import { Song } from './interfaces/song.interface';
+import { Artist } from './interfaces/artist.interface';
 
 @Component({
   selector: 'muzik-root',
@@ -16,10 +19,62 @@ import { PlayerComponent } from './player/player.component';
     EventSliderComponent,
     SongSliderComponent,
     PlayerComponent,
+    SamuraiButtonDirective,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'muzik';
+
+  favoriteSongs: Song[] = [
+    {
+      id: '1',
+      title: 'Khab Nabashim',
+      artist: 'Shayea',
+      image: 'assets/song-images/amadebash.jpg',
+    },
+    {
+      id: '2',
+      title: 'Khab Nabashim',
+      artist: 'Shayea',
+      image: 'assets/song-images/amadebash.jpg',
+    },
+    {
+      id: '3',
+      title: 'Khab Nabashim',
+      artist: 'Shayea',
+      image: 'assets/song-images/amadebash.jpg',
+    },
+    {
+      id: '4',
+      title: 'Khab Nabashim',
+      artist: 'Shayea',
+      image: 'assets/song-images/amadebash.jpg',
+    },
+  ];
+
+  favoriteArtists: Artist[] = [
+    {
+      id: '1',
+      firstName: 'mohammad reza',
+      lastName: 'shayea',
+      artisticName: 'shayea',
+      image: 'assets/artist-images/shayea.jpg',
+    },
+    {
+      id: '2',
+      firstName: 'mohammad reza',
+      lastName: 'shayea',
+      artisticName: 'shayea',
+      image: 'assets/artist-images/shayea.jpg',
+    },
+    {
+      id: '3',
+      firstName: 'mohammad reza',
+      lastName: 'shayea',
+      artisticName: 'shayea',
+      image: 'assets/artist-images/shayea.jpg',
+    },
+  ];
 }
